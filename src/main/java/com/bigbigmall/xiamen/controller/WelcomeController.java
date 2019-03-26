be482bf681d1f59009b79b78ba488261e0cce3e6
2662729661@qq.com <2662729661@qq.com>
2019-03-26 17:08:23 +0800

    test

--- src/main/java/com/bigbigmall/xiamen/controller/WelcomeController.java
+++ src/main/java/com/bigbigmall/xiamen/controller/WelcomeController.java
@@ -3,6 +3,7 @@ package com.bigbigmall.xiamen.controller;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,7 +19,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Text;
 
 /**
  * @author lian
@@ -27,6 +27,121 @@ import org.w3c.dom.Text;
 @RequestMapping("/")
 public class WelcomeController {
 
+        /*---------------------------------stream↓以下测试-------------------------------------------------*/
+        @RequestMapping("/stream")
+        @ResponseBody
+        public JSONArray step() {
+                JSONArray array = new JSONArray();
+                IntStream intStream = IntStream.range(2, 10);// 获取乘积
+                IntStream intStream2 = IntStream.range(2, 10);// 获取乘數
+                IntStream intStream3 = IntStream.range(2, 10);// 获取被乘數
+                IntStream flatMap = intStream.flatMap(this::multiplication);//获取乘积结果
+                IntStream flatMap2 = intStream2.flatMap(this::multiplication2);// 获取乘數结果
+                IntStream flatMap3 = intStream3.flatMap(this::multiplication3);// 获取被乘數结果
+                int[] toArray = flatMap.toArray();
+                int[] toArray2 = flatMap2.toArray();
+                int[] toArray3 = flatMap3.toArray();
+                for (int i = 0; i < toArray.length; i++) {
+                        JSONObject object = new JSONObject();
+                        object.put("mt1", toArray3[i]);
+                        object.put("mt2", toArray2[i]);
+                        object.put("mt3", toArray[i]);
+                        array.put(object);
+                }
+                //String string = array.toString();
+                return array;
+        }
+
+        //获取乘积
+        private IntStream multiplication(int input) {
+                return IntStream.range(1, 10).map(k -> input * k);
+        }
+
+        //获取乘數
+        private IntStream multiplication2(int input) {
+                return IntStream.range(1, 10).map(k -> input);
+        }
+
+        //获取被乘數
+        private IntStream multiplication3(int input) {
+                return IntStream.range(1, 10).map(k -> k);
+        }
+
+        /**
+         * 九九乘法表-XML 测试
+         *
+         * @return
+         */
+        @RequestMapping("/streamXML")
+        @ResponseBody
+        public ModelAndView getStreamXml(HttpServletResponse response) throws Exception {
+                //获取文档对象
+                DocumentBuilderFactory newInstance = DocumentBuilderFactory.newInstance();
+                DocumentBuilder newDocumentBuilder = newInstance.newDocumentBuilder();
+                Document doc = newDocumentBuilder.newDocument();
+
+                //创建根节点
+                Element documentElement = doc.createElement("document");
+                doc.appendChild(documentElement);
+
+                //获取数据
+                JSONArray mtArray = step();
+
+                //添加2-5
+                getMts(doc, mtArray, 0, 9, documentElement);
+
+                //添加6-9
+                getMts(doc, mtArray, 36, 45, documentElement);
+
+                Source source = new DOMSource(doc);
+                // 将XML源文件添加到模型中，以便XsltView能够检测
+                ModelAndView model = new ModelAndView("multiplication");
+                model.addObject("xmlSource", source);
+
+                return model;
+                // TransformerFactory.newInstance().newTransformer().transform(new DOMSource(doc), new StreamResult(response.getOutputStream()));
+        }
+
+        //添加mts
+        public Element getMts(Document doc, JSONArray mtArray, int i, int q, Element documentElement) {
+                for (int j = i; j < q; j++) {
+                        Element mtsElement = doc.createElement("mts");
+                        documentElement.appendChild(mtsElement);
+                        JSONObject object = mtArray.getJSONObject(j);
+                        Element element = getMs(doc, object);
+                        mtsElement.appendChild(element);
+                        JSONObject object2 = mtArray.getJSONObject(j + 9);
+                        Element element2 = getMs(doc, object2);
+                        mtsElement.appendChild(element2);
+                        JSONObject object3 = mtArray.getJSONObject(j + 18);
+                        Element element3 = getMs(doc, object3);
+                        mtsElement.appendChild(element3);
+                        JSONObject object4 = mtArray.getJSONObject(j + 27);
+                        Element element4 = getMs(doc, object4);
+                        mtsElement.appendChild(element4);
+                }
+
+                return documentElement;
+        }
+
+        //添加ms
+        public Element getMs(Document doc, JSONObject object) {
+                Element msElement = doc.createElement("ms");
+
+                Element mt1Element = doc.createElement("mt1");
+                Element mt2Element = doc.createElement("mt2");
+                Element mt3Element = doc.createElement("mt3");
+                mt1Element.appendChild(doc.createTextNode(object.get("mt2").toString() + " "));
+                mt2Element.appendChild(doc.createTextNode(object.get("mt1").toString() + " = "));
+                mt3Element.appendChild(doc.createTextNode(object.get("mt3").toString()));
+                msElement.appendChild(mt1Element);
+                msElement.appendChild(mt2Element);
+                msElement.appendChild(mt3Element);
+
+                return msElement;
+        }
+
+        /*---------------------------------stream↑以上测试-------------------------------------------------*/
         /**
          * 九九乘法表-xml 2019-3-24 当前练习
          *
@@ -35,7 +150,6 @@ public class WelcomeController {
         @RequestMapping("/multiplicationXml")
         @ResponseBody
         public JSONArray getMultiplicationTableXML() {
-
                 JSONArray array = new JSONArray();
 
                 //获取2-5的值
